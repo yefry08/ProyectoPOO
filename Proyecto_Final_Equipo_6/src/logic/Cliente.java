599ac8485c707dc34285e577fed390d9fbc162e2
Jean Luna <Jean Luna@192.168.56.1>
2023-07-21 10:53:55 -0400

    Se crearon los atributos de la clase cliente como prueba

--- Proyecto_Final_Equipo_6/src/logic/Cliente.java
+++ Proyecto_Final_Equipo_6/src/logic/Cliente.java
@@ -1,5 +1,11 @@
 package logic;
 
 public class Cliente {
+	
+	private String nombre;
+	private String apellido;
+	private String direccion;
+	private String telefono;
+	private Factura factura;
 
 }
